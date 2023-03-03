48f5f50b32cce064cbd7b08f178f169849797f60
Jinashree Gane <113277171+Jinny45@users.noreply.github.com>
2023-03-03 16:31:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
 //your code here!
+const list = document.getElementById('infi-list');
+
+function infinteScroll() {
+	
+
+  if (list.scrollTop + list.clientHeight >= list.scrollHeight) {
+    // user has reached the end of the list
+    const newItem1 = document.createElement('li');
+    newItem1.textContent = 'Item ' + (list.children.length + 1);
+    const newItem2 = document.createElement('li');
+    newItem2.textContent = 'Item ' + (list.children.length + 2);
+    list.appendChild(newItem1);
+    list.appendChild(newItem2);
+  }
+};
+list.addEventListener('scroll', infinteScroll)
 
